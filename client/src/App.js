73bdd92d6589c05c903b73bba4c30546e7d7c15e
Yunhwan <79241793+yunhwan98@users.noreply.github.com>
2023-04-18 01:26:27 +0900

    Revert "Style: 전역 스타일링 설정"

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,5 @@
-import Question from './pages/Question';
-import GlobalStyles from './styles/GlobalStyles.style';
-
 function App() {
-	return (
-		<>
-			<GlobalStyles />
-			<div>프로젝트 초기세팅</div>;
-		</>
-	);
+	return <div>프로젝트 초기세팅</div>;
 }
 
 export default App;
