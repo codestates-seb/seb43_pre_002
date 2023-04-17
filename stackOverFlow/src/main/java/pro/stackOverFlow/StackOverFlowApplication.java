2584c30039906bd5fdbdb478a10ada6080cf76cb
ksr0818 <120084774+ksr0818@users.noreply.github.com>
2023-04-17 17:17:09 +0900

    feat 백엔드 초기 세팅

---
+++ stackOverFlow/src/main/java/pro/stackOverFlow/StackOverFlowApplication.java
@@ -0,0 +1,13 @@
+package pro.stackOverFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackOverFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StackOverFlowApplication.class, args);
+	}
+
+}
---
+++ stackOverFlow/src/test/java/pro/stackOverFlow/StackOverFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.stackOverFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StackOverFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
