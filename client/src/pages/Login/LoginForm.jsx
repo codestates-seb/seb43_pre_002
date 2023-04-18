9fda0ecfcb869fd469dde4bf482c04366a07bc05
Seulgi-Yoo <ysg213@naver.com>
2023-04-18 18:56:45 +0900

    Feat: Login Form 컴포넌트 추가

---
+++ client/src/pages/Login/LoginForm.jsx
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+import SignInput from '../../components/Input/SignInput';
+import SignButton from '../../components/Button/SignButton';
+
+const FormContainer = styled.div`
+	padding: 24px;
+	background-color: #fff;
+	border-radius: 5px;
+	box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.1);
+`;
+
+function LoginForm() {
+	return (
+		<FormContainer>
+			<form>
+				<SignInput labelFor="email" labelName="Email" inputType="email" />
+				<SignInput
+					labelFor="password"
+					labelName="Password"
+					inputType="password"
+				/>
+				<SignButton>Log in</SignButton>
+			</form>
+		</FormContainer>
+	);
+}
+
+export default LoginForm;
