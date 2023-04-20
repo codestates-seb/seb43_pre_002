6a645093b461b9918e1eaba2ac88833ab851e62a
Seulgi-Yoo <ysg213@naver.com>
2023-04-20 15:49:09 +0900

    Refactor: 컴포넌트 분리

---
+++ client/src/components/Agreement.jsx
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+
+const AgreementContainer = styled.div`
+	display: flex;
+	flex-wrap: wrap;
+	align-items: center;
+	margin: 6px 0;
+	font-size: var(--font-small);
+`;
+
+const ErrorMessage = styled.p`
+	font-size: var(--font-small);
+	color: var(--error-message-color);
+`;
+
+function Agreement({
+	labelName,
+	labelFor,
+	inputType,
+	register,
+	registerOptions,
+	errors,
+}) {
+	return (
+		<AgreementContainer>
+			<input
+				type={inputType}
+				name={labelFor}
+				id={labelFor}
+				{...register(labelFor, registerOptions)}
+			/>
+			<label htmlFor={labelFor}>{labelName}</label>
+			{errors && <ErrorMessage>{errors.message}</ErrorMessage>}
+		</AgreementContainer>
+	);
+}
+
+export default Agreement;
