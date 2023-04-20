da60711bd4c06e1a38f160d6e105c67a18747f71
kimsh322 <kimsh91322@gmail.com>
2023-04-20 13:16:07 +0900

    Feat : Home,AskQuestion 라우터 추가

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import GlobalStyles from './styles/GlobalStyles.style';
 import MyEdit from './pages/mypage/myedit';
 import MyProfile from './pages/mypage/myprofile';
 import MyActivity from './pages/mypage/myactivity';
+import Home from './pages/Home';
+import AskQuestion from './pages/AskQuestion';
 
 function App() {
 	return (
@@ -10,6 +12,8 @@ function App() {
 			<GlobalStyles />
 			<BrowserRouter>
 				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/askquestion" element={<AskQuestion />} />
 					<Route path="/myprofile" element={<MyProfile />} />
 					<Route path="/myactivity" element={<MyActivity />} />
 					<Route path="/myedit" element={<MyEdit />} />
