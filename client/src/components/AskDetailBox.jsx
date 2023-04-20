dc6171691062e88ff3ad68d05f5ec25337aeb8a0
SuHyeon Kim <118786973+kimsh322@users.noreply.github.com>
2023-04-20 10:24:04 +0900

    Feat: 질문 작성페이지 UI 구현 (#19)
    * Chore: styled-reset 설치
    
    * Style: 전역 스타일링 설정
    
    * Fix: 오류 구문수정
    
    * Feat : 질문작성 페이지 first commit
    
    * Feat : Ask헤더, Ask 설명, Ask Title UI부분 작성
    
    Ask헤더, Ask 설명, Ask Title UI 부분 작성
    
    * Feat : Ask 페이지 1차 완성
    
    * Fix : 파일이름 변경, AskQuestion 페이지 배경색 변경
    
    ---------
    
    Co-authored-by: yunhwan <sky10240107@naver.com>
    Co-authored-by: Seulgi-Yoo <119473025+Seulgi-Yoo@users.noreply.github.com>

---
+++ client/src/components/AskDetailBox.jsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+import { useState } from 'react';
+import AskDetail from './AskDetail';
+import AskHelpler from './AskHelper';
+import { detailHelperContents } from './askHelperContents';
+
+function AskDetailBox() {
+	const [isDetailFocus, setIsDetailFocus] = useState(false);
+	return (
+		<AskDetailBoxContainer>
+			<AskDetail setIsDetailFocus={setIsDetailFocus} />
+			{isDetailFocus ? <AskHelpler contents={detailHelperContents} /> : null}
+		</AskDetailBoxContainer>
+	);
+}
+
+export default AskDetailBox;
+
+const AskDetailBoxContainer = styled.div`
+	display: flex;
+	width: 100%;
+	margin-bottom: 1%;
+`;
