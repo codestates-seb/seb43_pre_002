a94a002b334f33c8b9e453af28c6bda375f3c4c2
Seulgi-Yoo <ysg213@naver.com>
2023-04-20 15:49:49 +0900

    Feat: 전역 색상 추가

--- client/src/styles/GlobalStyles.style.js
+++ client/src/styles/GlobalStyles.style.js
@@ -11,7 +11,7 @@ const GlobalStyles = createGlobalStyle`
         --font-color-gray: #BABFC3;
         --modal-color: #E5F3FF;
         --button-hover-color: #3B6FA0;
-        --font-color-gray: #BABFC3;
+        --error-message-color: #CB2A2A;
 
         --font-title-large: 27px;
         --font-title-small: 17px;
