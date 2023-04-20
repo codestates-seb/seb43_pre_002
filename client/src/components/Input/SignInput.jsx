221318d0acf7eacb43f9ab20014839e828ef4991
Seulgi-Yoo <ysg213@naver.com>
2023-04-20 16:31:45 +0900

    Feat: 유효성 체크 기능 추가

--- client/src/components/Input/SignInput.jsx
+++ client/src/components/Input/SignInput.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { AiFillExclamationCircle } from 'react-icons/ai';
 
 const InputContainer = styled.div`
 	display: flex;
@@ -14,14 +15,55 @@ const InputContainer = styled.div`
 		border-radius: 3px;
 		height: 30px;
 		border: 1px solid var(--line-color);
+		&:focus {
+			outline: none;
+			border: 2px solid var(--main-color);
+		}
+	}
+	input.error {
+		border-color: var(--error-message-color);
+		&:focus {
+			border: 2px solid var(--error-message-color);
+		}
 	}
 `;
 
-function SignInput({ labelName, labelFor, inputType }) {
+const ErrorMessage = styled.p`
+	margin-top: 1px;
+	display: flex;
+	align-items: center;
+	font-size: var(--font-small);
+	color: var(--error-message-color);
+`;
+
+const ErrorIcon = styled(AiFillExclamationCircle)`
+	margin-right: 5px;
+`;
+
+function SignInput({
+	labelName,
+	labelFor,
+	inputType,
+	register,
+	registerOptions,
+	error,
+}) {
 	return (
 		<InputContainer>
 			<label htmlFor={labelFor}>{labelName}</label>
-			<input type={inputType} name={labelFor} id={labelFor} required />
+			<input
+				className={error ? 'error' : ''}
+				type={inputType}
+				name={labelFor}
+				id={labelFor}
+				{...register(labelFor, registerOptions)}
+			/>
+			{error && (
+				<ErrorMessage>
+					<ErrorIcon />
+					{error.message}
+				</ErrorMessage>
+			)}
 		</InputContainer>
 	);
 }
--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -1,6 +1,8 @@
+import { useForm } from 'react-hook-form';
 import styled from 'styled-components';
 import SignInput from './Input/SignInput';
 import SignButton from './Button/SignButton';
+import Agreement from './Agreement';
 
 const FormContainer = styled.div`
 	padding: 24px;
@@ -14,43 +16,77 @@ const PasswordText = styled.p`
 	color: #727981;
 `;
 
-const Agreement = styled.div`
-	display: flex;
-	align-items: center;
-	margin: 6px 0;
-	font-size: var(--font-small);
-`;
-
 function SignupForm() {
+	const onSubmit = (data) => {
+		// eslint-disable-next-line no-console
+		console.log(data);
+	};
+
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm();
+
 	return (
 		<FormContainer>
-			<form>
+			<form onSubmit={handleSubmit(onSubmit)}>
 				<SignInput
-					labelFor="display-name"
+					labelFor="displayname"
 					labelName="Display name"
 					inputType="text"
+					register={register}
+					registerOptions={{
+						required: 'Display cannot be empty',
+					}}
+					error={errors.displayname}
 				/>
-				<SignInput labelFor="email" labelName="Email" inputType="email" />
+				<SignInput
+					labelFor="email"
+					labelName="Email"
+					inputType="email"
+					register={register}
+					registerOptions={{
+						required: 'Email cannot be empty',
+						pattern: {
+							value: /\S+@\S+\.\S+/,
+							message: 'Invalid email format',
+						},
+					}}
+					error={errors.email}
+				/>
 				<SignInput
 					labelFor="password"
 					labelName="Password"
 					inputType="password"
+					register={register}
+					registerOptions={{
+						required: 'Password cannot be empty',
+						minLength: {
+							value: 8,
+							message: 'Password should have at least 8 characters',
+						},
+						pattern: {
+							value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+							message:
+								'Password should contain at least one letter and one number',
+						},
+					}}
+					error={errors.password}
 				/>
 				<PasswordText>
 					Passwords must contain at least eight characters, including at least 1
 					letter and 1 number.
 				</PasswordText>
-				<Agreement>
-					<div>
-						<input type="checkbox" name="agreement" id="agreement" />
-					</div>
-					<div>
-						{/* label input 연결 했음에도 오류 발생하여 해당 line eslint 무시 */}
-						{/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-						<label htmlFor="agreement">회원 가입에 동의합니다.</label>
-					</div>
-				</Agreement>
-				<SignButton>Sign up</SignButton>
+				<Agreement
+					labelName="회원 가입에 동의합니다."
+					labelFor="agreement"
+					inputType="checkbox"
+					register={register}
+					registerOptions={{ required: '회원 가입 약관에 동의해주세요.' }}
+					error={errors.agreement}
+				/>
+				<SignButton type="submit">Sign up</SignButton>
 			</form>
 		</FormContainer>
 	);
