e85625cb5b193eba9088a15ded231b1877f2db5c
zi0ne <jiwon0331j@gmail.com>
2023-04-21 01:59:06 +0900

    Feat: 마이페이지 액티비티 게시물 리스트 기능구현

--- client/src/pages/mypage/myactivity.jsx
+++ client/src/pages/mypage/myactivity.jsx
@@ -1,14 +1,38 @@
+/* eslint-disable camelcase */
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import styled, { createGlobalStyle } from 'styled-components';
 import MyHeader from '../../components/MyHeader';
 import MyList from '../../components/MyList';
 
-const lists1 = Array(5).fill({
-	answerCount: 0,
-	link: 'https://stackoverflow.com/',
-	date: 'YYYY.MM.DD',
-});
-
 function MyActivity() {
+	const [userData, setUserData] = useState({});
+	const [articleData, setArticleData] = useState([]);
+	const { member_id } = useParams();
+
+	useEffect(() => {
+		const fetchData = async () => {
+			const result = await axios.get(`http://localhost:3000/member`);
+			setUserData(result.data);
+
+			const articleResult = await axios.get(`http://localhost:3000/question`);
+			setArticleData(articleResult.data);
+		};
+		fetchData();
+	}, []);
+
+	const filteredArticles = articleData
+		? articleData.filter((a) => a.memberId === parseInt(member_id, 10))
+		: [];
+
+	const filteredAnswerd = articleData
+		? articleData.filter((a) => a.answerId === parseInt(member_id, 10))
+		: [];
+
+	const sortedArticles = filteredArticles.sort((a, b) => b.answer - a.answer);
+	const sortedAnswers = filteredAnswerd.sort((a, b) => b.answer - a.answer);
+
 	return (
 		<Wrap>
 			<GlobalStyles />
@@ -17,15 +41,15 @@ function MyActivity() {
 				<Post>
 					<PostWrap>
 						<Category>Ansewer</Category>
-						<MyList lists={lists1} />
+						<MyList lists={sortedAnswers.slice(0, 5)} />
 					</PostWrap>
 					<PostWrap>
 						<Category>Question</Category>
-						<MyList lists={lists1} />
+						<MyList lists={sortedArticles.slice(0, 5)} />
 					</PostWrap>
 				</Post>
 				<Category2>Account</Category2>
-				<MyList lists={lists1} />
+				<MyList lists={sortedArticles.slice(0, 5)} />
 			</ListColumn>
 		</Wrap>
 	);
