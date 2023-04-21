d7820503026c9aa072379ac55a68badb347326fd
zi0ne <jiwon0331j@gmail.com>
2023-04-22 02:33:46 +0900

    Feat: 유저페이지 프로필  기능 구현

--- client/src/pages/user/userprofile.jsx
+++ client/src/pages/user/userprofile.jsx
@@ -1,32 +1,55 @@
+/* eslint-disable camelcase */
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import styled, { createGlobalStyle } from 'styled-components';
 import UserHeader from '../../components/UserHeader';
 import MyList from '../../components/MyList';
 
-const lists2 = Array(10).fill({
-	answerCount: 0,
-	link: 'https://stackoverflow.com/',
-	date: 'YYYY.MM.DD',
-});
-
 function UserProfile() {
+	const [userData, setUserData] = useState({});
+	const [articleData, setArticleData] = useState([]);
+	const { member_id } = useParams();
+
+	useEffect(() => {
+		const fetchData = async () => {
+			const result = await axios.get(` http://localhost:3000/data`);
+			setUserData(result.data);
+
+			const articleResult = await axios.get(`http://localhost:3000/question`);
+			setArticleData(articleResult.data);
+		};
+		fetchData();
+	}, []);
+
+	const filteredArticles = articleData
+		? articleData.filter((a) => a.memberId === parseInt(member_id, 10))
+		: [];
+
+	const sortedArticles = filteredArticles.sort((a, b) => b.answer - a.answer);
+
+	function isEmpty(value) {
+		return value === null || value === undefined || value === '';
+	}
+
 	return (
 		<Wrap>
 			<GlobalStyles />
 			<UserHeader />
-			<div>
-				<Category>About</Category>
-				<AboutBox>
-					유저가 작성한 Aboutme 내용이 들어갑니다.유저가 About me에 작성한
-					내용이 없을경우 hidden처리 합니다.
-				</AboutBox>
-			</div>
+			{userData && !isEmpty(userData[`${member_id - 1}`]?.aboutMe) && (
+				<div>
+					<Category>About</Category>
+					<AboutBox>{userData[`${member_id - 1}`]?.aboutMe}</AboutBox>
+				</div>
+			)}
 			<Post>
 				<Category>Top posts</Category>
-				<MyList lists={lists2} />
+				<MyList lists={sortedArticles.slice(0, 10)} />
 			</Post>
 		</Wrap>
 	);
 }
+
 const Wrap = styled.div`
 	margin-top: 40px;
 	display: flex;
@@ -42,7 +65,7 @@ const AboutBox = styled.p`
 	margin-top: 20px;
 	border-radius: 5px;
 	text-justify: center;
-	padding-top: 5px;
+	padding: 10px 5px;
 `;
 const Category = styled.span`
 	font-size: 21px;
