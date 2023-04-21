0566e1542c3e0ed724cb6a12a704ee41fb94d258
Seulgi-Yoo <ysg213@naver.com>
2023-04-21 09:52:10 +0900

    Feat: 유효성 체크 에러 메세지 추가

--- client/src/components/Agreement.jsx
+++ client/src/components/Agreement.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { AiFillExclamationCircle } from 'react-icons/ai';
 
 const AgreementContainer = styled.div`
 	display: flex;
@@ -6,11 +7,21 @@ const AgreementContainer = styled.div`
 	align-items: center;
 	margin: 6px 0;
 	font-size: var(--font-small);
+	input {
+		margin-left: 0;
+	}
 `;
 
 const ErrorMessage = styled.p`
+	margin-top: 1px;
+	display: flex;
+	align-items: center;
 	font-size: var(--font-small);
 	color: var(--error-message-color);
+`;
+
+const ErrorIcon = styled(AiFillExclamationCircle)`
+	margin-right: 5px;
 `;
 
 function Agreement({
@@ -30,7 +41,12 @@ function Agreement({
 				{...register(labelFor, registerOptions)}
 			/>
 			<label htmlFor={labelFor}>{labelName}</label>
-			{error && <ErrorMessage>{error.message}</ErrorMessage>}
+			{error && (
+				<ErrorMessage>
+					<ErrorIcon />
+					{error.message}
+				</ErrorMessage>
+			)}
 		</AgreementContainer>
 	);
 }
