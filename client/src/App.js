7a2b951f827069315ff52b854adb18745a6f5fb1
Seulgi-Yoo <ysg213@naver.com>
2023-04-23 19:01:25 +0900

    Fix: 헤더, 라우트 페이지 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
 import GlobalStyles from './styles/GlobalStyles.style';
 import MyEdit from './pages/mypage/myedit';
 import MyProfile from './pages/mypage/myprofile';
@@ -6,19 +7,24 @@ import MyActivity from './pages/mypage/myactivity';
 import Signup from './pages/Signup/Signup';
 import Login from './pages/Login/Login';
 import Home from './pages/Home';
+import LoginHeader from './components/Header/LoginHeader';
+import SignupSuccess from './pages/SignupSuccess/SignupSuccess';
 
 function App() {
+	const [isLogin, setIsLogin] = useState(false);
 	return (
 		<div>
 			<GlobalStyles />
 			<BrowserRouter>
+				<LoginHeader isLogin={isLogin} setIsLogin={setIsLogin} />
 				<Routes>
 					<Route path="/" element={<Home />} />
 					<Route path="/myprofile" element={<MyProfile />} />
 					<Route path="/myactivity" element={<MyActivity />} />
 					<Route path="/myedit" element={<MyEdit />} />
 					<Route path="/signup" element={<Signup />} />
-					<Route path="/login" element={<Login />} />
+					<Route path="/signupsuccess" element={<SignupSuccess />} />
+					<Route path="/login" element={<Login setIsLogin={setIsLogin} />} />
 				</Routes>
 			</BrowserRouter>
 		</div>
