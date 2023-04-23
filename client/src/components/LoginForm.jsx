be7f7c0e9a0a091b777c221b0dbcda4bb3953f2b
Seulgi-Yoo <jinjutin@gmail.com>
2023-04-23 14:34:18 +0900

    Fix: 로그인 기능 구현중

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
@@ -13,44 +14,34 @@ const FormContainer = styled.div`
 	box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.1);
 `;
 
-function LoginForm({
-	isLogin,
-	setIsLogin,
-	userInfo,
-	setUserInfo,
-	setLoginError,
-}) {
+function LoginForm({ setIsLogin, setLoginError }) {
 	const navigate = useNavigate();
 
-	const onSubmit = (data) => {
-		// navigate('/');
-
-		axios
-			.post(`/auth/login`, data) // package.json proxy url 확인
+	const onSubmit = async (loginInfo) => {
+		await axios
+			.post(`/auth/login`, loginInfo) // package.json proxy url 확인
 			.then((response) => {
 				// console.log(response);
-				const { accessToken } = response.data;
-				localStorage.setItem('accessToken', accessToken); // 로그아웃 시 removeItem
+				const accessToken = response.headers.authorization;
+				const localAccessToken = localStorage.getItem('access_token');
+				if (!localAccessToken) {
+					localStorage.setItem('access_token', accessToken); // 로그아웃 시 removeItem
+				}
+				axios.defaults.headers.common.Authorization = `Bearer ${localAccessToken}`;
+				return response.data;
+			})
+			.then((data) => {
+				localStorage.setItem('user', JSON.stringify(data.user)); // 유저 정보를 로컬 스토리지에 저장
 				setIsLogin(true);
-				setUserInfo(response.data);
-				setLoginError(null);
 				navigate('/');
-				// ! 서버 측과 로그인 인증 방식: JWT
 			})
 			.catch((error) => {
 				setLoginError(
 					'로그인에 실패했습니다. 이메일과 비밀번호를 확인해주세요.',
 				);
-				console.log(error.response.data);
+				// console.log(error.response.data);
 			});
 	};
-	// 로그인 후 JWT가 있으면 모든 axios 요청에 토큰을 전송하는 인터셉터 설정
-	// axios.interceptors.request.use((config) => {
-	// 	if (token) {
-	// 		config.headers.Authorization = `Bearer ${token}`;
-	// 	}
-	// 	return config;
-	// });
 
 	const {
 		register,
@@ -60,37 +51,33 @@ function LoginForm({
 
 	return (
 		<FormContainer>
-			{isLogin ? (
-				<div>{userInfo}</div>
-			) : (
-				<form onSubmit={handleSubmit(onSubmit)}>
-					<SignInput
-						labelFor="email"
-						labelName="Email"
-						inputType="email"
-						register={register}
-						registerOptions={{
-							required: 'Email cannot be empty',
-							pattern: {
-								value: /\S+@\S+\.\S+/,
-								message: 'Invalid email format',
-							},
-						}}
-						error={errors.email}
-					/>
-					<SignInput
-						labelFor="password"
-						labelName="Password"
-						inputType="password"
-						register={register}
-						registerOptions={{
-							required: 'Password cannot be empty',
-						}}
-						error={errors.password}
-					/>
-					<SignButton>Log in</SignButton>
-				</form>
-			)}
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<SignInput
+					labelFor="email"
+					labelName="Email"
+					inputType="email"
+					register={register}
+					registerOptions={{
+						required: 'Email cannot be empty',
+						pattern: {
+							value: /\S+@\S+\.\S+/,
+							message: 'Invalid email format',
+						},
+					}}
+					error={errors.email}
+				/>
+				<SignInput
+					labelFor="password"
+					labelName="Password"
+					inputType="password"
+					register={register}
+					registerOptions={{
+						required: 'Password cannot be empty',
+					}}
+					error={errors.password}
+				/>
+				<SignButton>Log in</SignButton>
+			</form>
 		</FormContainer>
 	);
 }
