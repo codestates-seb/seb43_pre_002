93d2d649ada8ad8ea66e8d64e8b1b411c632a344
zi0ne <jiwon0331j@gmail.com>
2023-04-24 12:05:21 +0900

    Fix: 충돌해결1

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import GlobalStyles from './styles/GlobalStyles.style';
+import MyEdit from './pages/mypage/myedit';
+import MyProfile from './pages/mypage/myprofile';
+import MyActivity from './pages/mypage/myactivity';
 import UserActivity from './pages/user/useractivity';
 import UserProfile from './pages/user/userprofile';
 
 function App() {
 	return (
 		<div>
+			<GlobalStyles />
 			<BrowserRouter>
 				<Routes>
+					<Route path="/myprofile" element={<MyProfile />} />
+					<Route path="/myactivity" element={<MyActivity />} />
+					<Route path="/myedit" element={<MyEdit />} />
 					<Route path="/useractivity/:member_id" element={<UserActivity />} />
 					<Route path="/userprofile/:member_id" element={<UserProfile />} />
 				</Routes>
