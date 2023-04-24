89ddbeb970f27fd6586dccdb15b0200793b17354
Seulgi-Yoo <119473025+Seulgi-Yoo@users.noreply.github.com>
2023-04-24 11:29:21 +0900

    헤더 컴포넌트 구조 변경 (#51)
    * Feat: 각 페이지로 라우팅 연결
    
    * Fix: 헤더 컴포넌트 구조 수정
    하나의 헤더 컴포넌트 내에서 isLogin 상태에 따라 버튼을 조건부 렌더링 하는 코드로 변경했습니다.
    
    ---------
    
    Co-authored-by: Seulgi-Yoo <jinjutin@gmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,29 @@
-
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import GlobalStyles from './styles/GlobalStyles.style';
 import MyEdit from './pages/mypage/myedit';
 import MyProfile from './pages/mypage/myprofile';
 import MyActivity from './pages/mypage/myactivity';
+import Home from './pages/Home';
+import Login from './pages/Login/Login';
+import Signup from './pages/Signup/Signup';
+import LoginHeader from './components/Header/LoginHeader';
 
 function App() {
 	return (
 		<div>
 			<GlobalStyles />
 			<BrowserRouter>
+				<LoginHeader />
 				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/signup" element={<Signup />} />
 					<Route path="/myprofile" element={<MyProfile />} />
 					<Route path="/myactivity" element={<MyActivity />} />
 					<Route path="/myedit" element={<MyEdit />} />
 				</Routes>
 			</BrowserRouter>
 		</div>
-
 	);
 }
 
