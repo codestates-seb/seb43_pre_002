a8adb46f3f31cbbf184c0503af1c9545b1d8dd87
ksr0818 <120084774+ksr0818@users.noreply.github.com>
2023-04-24 14:37:34 +0900

    update MemberMapper(Mapping 관련)

--- server/src/main/java/pro/stackOverFlow/member/mapper/MemberMapper.java
+++ server/src/main/java/pro/stackOverFlow/member/mapper/MemberMapper.java
@@ -4,6 +4,8 @@ package pro.stackOverFlow.member.mapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
+import pro.stackOverFlow.answer.dto.AnswerMyPageDto;
+import pro.stackOverFlow.answer.entity.Answer;
 import pro.stackOverFlow.member.dto.MemberDto;
 import pro.stackOverFlow.member.dto.MemberMyPageDto;
 import pro.stackOverFlow.member.entity.Member;
@@ -14,8 +16,6 @@ import pro.stackOverFlow.question.entity.Question;
 import java.util.List;
 import java.util.stream.Collectors;
 
-//import static java.util.stream.Nodes.collect;
-
 @Mapper(componentModel = "spring")
 public interface MemberMapper {
 
@@ -25,42 +25,34 @@ public interface MemberMapper {
     MemberDto.Response memberToMemberResponseDto(Member member);
     List<MemberDto.Response> membersToMemberResponseDtos(List<Member> members);
 
-    default MemberMyPageDto memberToMemberMyPageDto(Member member) { // 완료
+    default MemberMyPageDto memberToMemberMyPageDto(Member member) {
+
         if (member == null) {
             return null;
         }
+
         MemberMyPageDto memberMyPageDto = new MemberMyPageDto();
         memberMyPageDto.setMemberId(member.getMemberId());
         memberMyPageDto.setEmail(member.getEmail());
         memberMyPageDto.setDisplayName(member.getDisplayName());
         memberMyPageDto.setTitle(member.getTitle());
         memberMyPageDto.setAboutMe(member.getAboutMe());
-//        List<Question> questions = member.getQuestions(); // 질문 리스트
 
-//        List<AnswerMyPageDto> answerMyPageDtoList = answerList.stream().map(answer ->{
-//            AnswerMyPageDto answerMyPageDto = new AnswerMyPageDto();
-//            answerMyPageDto.setAnswerId(answer.getAnswerId());
-//            answerMyPageDto.setTitle(answer.getQuestion().getTitle());
-//            answerMyPageDto.setCreatedAt(answer.getCreatedAt());
-//            answerMyPageDto.setAnswerCount(answer.getQuestion().getAnswers().size());
-//            return answerMyPageDto;
 
-//        }
-//        ).collect(Collectors.toList());
-//        List<Answer> answerList = member.getAnswers(); // 답변 리스트
+        List<Answer> answerList = member.getAnswers();
 
-//        List<AnswerMyPageDto> answerMyPageDtoList = answerList.stream().map(answer ->{
-//            AnswerMyPageDto answerMyPageDto = new AnswerMyPageDto();
-//            answerMyPageDto.setAnswerId(answer.getAnswerId());
-//            answerMyPageDto.setTitle(answer.getQuestion().getTitle());
-//            answerMyPageDto.setCreatedAt(answer.getCreatedAt());
-//            answerMyPageDto.setAnswerCount(answer.getQuestion().getAnswers().size());
-//            return answerMyPageDto;
-//
-//        }).collect(Collectors.toList());
-//        memberMyPageDto.setAnswers(answerMyPageDtoList);
-//
-        List<Question> questionList = member.getQuestions(); // 질문 리스트
+        List<AnswerMyPageDto> answerMyPageDtoList = answerList.stream().map(answer ->{
+            AnswerMyPageDto answerMyPageDto = new AnswerMyPageDto();
+            answerMyPageDto.setAnswerId(answer.getAnswerId());
+            answerMyPageDto.setContent(answer.getContent());
+            answerMyPageDto.setCreatedAt(answer.getCreatedAt());
+            return answerMyPageDto;
+        }).collect(Collectors.toList());
+
+        memberMyPageDto.setAnswers(answerMyPageDtoList);
+
+        List<Question> questionList = member.getQuestions();
+
         List<QuestionMyPageDto> questionMyPageDtoList = questionList.stream().map(question ->{
             QuestionMyPageDto questionMyPageDto = new QuestionMyPageDto();
             questionMyPageDto.setQuestionId(question.getQuestionId());
@@ -69,6 +61,7 @@ public interface MemberMapper {
             questionMyPageDto.setCreatedAt(question.getMember().getCreatedAt());
             return questionMyPageDto;
         }).collect(Collectors.toList());
+
         memberMyPageDto.setQuestions(questionMyPageDtoList);
 
         return memberMyPageDto;
