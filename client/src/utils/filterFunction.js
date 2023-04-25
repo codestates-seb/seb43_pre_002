680e8d1318ac2f6003d8104107cd78eb000c86e9
yunhwan <sky10240107@naver.com>
2023-04-25 15:40:53 +0900

    Feat: 답변 정렬 함수 추가

--- client/src/utils/filterFunction.js
+++ client/src/utils/filterFunction.js
@@ -14,3 +14,25 @@ export const unansweredList = (arr) => {
 	const newArr = JSON.parse(JSON.stringify(arr));
 	return newArr.filter((el) => !el.answer);
 };
+
+export const newestAnswer = (arr) => {
+	const newArr = JSON.parse(JSON.stringify(arr));
+	newArr.sort((a, b) => {
+		return (
+			new Date(b.answerCreatedAt).getTime() -
+			new Date(a.answerCreatedAt).getTime()
+		);
+	});
+	return newArr;
+};
+
+export const oldestAnswer = (arr) => {
+	const newArr = JSON.parse(JSON.stringify(arr));
+	newArr.sort((a, b) => {
+		return (
+			new Date(a.answerCreatedAt).getTime() -
+			new Date(b.answerCreatedAt).getTime()
+		);
+	});
+	return newArr;
+};
