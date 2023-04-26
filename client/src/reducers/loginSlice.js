5eb5ceac651e5937ff408ecadd478cae6baeb36c
SuHyeon Kim <118786973+kimsh322@users.noreply.github.com>
2023-04-26 10:30:28 +0900

    Feat/ask 구현 (#76)
    * Feat : submit시 서버로 post요청하는 기능 추가
    
    * Feat : submit 버튼 클릭시 한번 더 확인하는 modal창 추가
    
    * Feat : 질문 작성 제출시 모달창 추가,에러처리 추가
    
    * Fix : proxy 서버 url 수정
    
    * Feat : redux기능 삽입, 질문글 제출기능 엔드포인트 수정
    
    * Fix : 시험용 코드 삭제

---
+++ client/src/reducers/loginSlice.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-param-reassign */
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+	isLogin: !!localStorage.getItem('loginMemberId'),
+};
+
+export const loginSlice = createSlice({
+	name: 'login',
+	initialState,
+	reducers: {
+		setIsLogin: (state, action) => {
+			state.isLogin = action.payload;
+			if (!action.payload) {
+				localStorage.removeItem('loginMemberId');
+			}
+		},
+	},
+});
+
+export const { setIsLogin } = loginSlice.actions;
+
+export const selectIsLogin = (state) => state.login.isLogin;
+
+export default loginSlice.reducer;
