5651204510ee785b4032cfc4706196255360cb71
ksr0818 <120084774+ksr0818@users.noreply.github.com>
2023-04-27 02:02:00 +0900

    fix : 불필요한 코드 삭제

--- server/src/main/java/pro/stackOverFlow/member/service/MemberService.java
+++ server/src/main/java/pro/stackOverFlow/member/service/MemberService.java
@@ -4,7 +4,7 @@ package pro.stackOverFlow.member.service;
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
 
-//import org.springframework.security.crypto.password.PasswordEncoder;
+
 import lombok.Getter;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,7 +13,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-//import pro.stackOverFlow.auth.utils.CustomAuthorityUtils;
 import pro.stackOverFlow.auth.utils.CustomAuthorityUtils;
 import pro.stackOverFlow.auth.utils.GetAuthUserUtils;
 import pro.stackOverFlow.exception.BusinessLogicException;
@@ -29,17 +28,12 @@ import javax.crypto.*;
 import java.nio.charset.StandardCharsets;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
-//import java.util.Base64;
 import java.util.Base64;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-/**
- *  - 메서드 구현
- *  - DI 적용
- *  - Spring Data JPA 적용
- */
+
 @Transactional
 @Service
 public class MemberService {
@@ -58,8 +52,6 @@ public class MemberService {
     }
     @SneakyThrows
     public Member createMember(Member member) {
-//        Member findMember = memberRepository.findByEmail(member.getEmail()).get();
-//        Member.checkExistEmail(member);
 
         String encryptedPassword = passwordEncoder.encode(member.getPassword());
         member.setPassword(encryptedPassword);
@@ -68,70 +60,6 @@ public class MemberService {
         member.setRoles(roles);
 
         return memberRepository.save(member);
-
-
-//        String key = "aaaa1111";
-//
-//
-//        String decryptedText;
-//
-//        ScriptEngineManager factory = new ScriptEngineManager();
-//        ScriptEngine engine = factory.getEngineByName("JavaScript");
-//        engine.eval("var ciphertext = '" + member.getPassword() + "';");
-//        engine.eval("var key = '" + key + "';");
-//        engine.eval("var plaintext = CryptoJS.AES.decrypt(ciphertext, key).toString(CryptoJS.enc.Utf8);");
-//        decryptedText = engine.get("plaintext").toString();
-//
-//        member.setPassword(decryptedText);
-
-//        //Todo : 비밀번호 복호화 !!
-//
-//        System.out.println("#Key :" + KEY);
-//
-//        String ALGORITHM = "AES";
-////        String KEY = "${decrypt.key}";
-//
-////        String secretKey = System.getenv("");
-//
-////         시크릿 키를 바이트 배열로 변환
-//        byte[] keyBytes = KEY.getBytes(StandardCharsets.UTF_8);
-//
-//        // SecretKeySpec 인스턴스 생성
-//        SecretKeySpec secretKeySpec = new SecretKeySpec(keyBytes, "AES");
-//
-//        // Cipher 인스턴스 초기화
-//        Cipher cipher = Cipher.getInstance("AES/ECB/NoPadding");
-//        cipher.init(Cipher.DECRYPT_MODE, secretKeySpec);
-//
-//        // 복호화할 암호문
-//        String encryptedText = member.getPassword();
-////        String encryptedText = "U2FsdGVkX18fuZ+IlY3JKdqjnpXbAJtBOM1BFeZ/L9E=";
-//
-//        // Base64 디코딩 후 암호문 복호화
-//        byte[] encryptedBytes = Base64.getDecoder().decode(encryptedText);
-//        byte[] decryptedBytes = cipher.doFinal(encryptedBytes);
-//
-//        // 복호화된 평문 출력
-//        String decryptedText = new String(decryptedBytes, StandardCharsets.UTF_8);
-//        System.out.println("#String : " + decryptedText);
-//        member.setPassword(decryptedText);
-
-
-//        String encryptedData = member.getPassword(); // 암호화된 데이터
-//        SecretKeySpec secretKey = new SecretKeySpec(KEY.getBytes(), ALGORITHM);
-//        Cipher cipher = Cipher.getInstance(ALGORITHM);
-//        cipher.init(Cipher.DECRYPT_MODE, secretKey);
-//        // 암호화된 데이터를 바이트 배열로 변환
-//        byte[] encryptedBytes = Base64.getDecoder().decode(encryptedData);
-//
-//        // 암호화된 데이터를 복호화
-//        byte[] decryptedBytes = cipher.doFinal(encryptedBytes);
-//
-//        // 복호화된 데이터를 문자열로 변환
-//        String decryptedData = new String(decryptedBytes);
-//
-//        member.setPassword(decryptedData);
-
     }
 
     public Member updateMember(Member member) {
@@ -196,11 +124,6 @@ public class MemberService {
             throw new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND);
         }
     }
-
-//    public static void checkExistEmail(Member targetMember) {
-//        if(targetMember != null)
-//            throw new BusinessLogicException(ExceptionCode.MEMBER_EXISTS);
-//    }
 
     public Member getLoginMember() {
         return  memberRepository.findByEmail(GetAuthUserUtils.getAuthUser().getName())
